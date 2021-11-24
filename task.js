2e303e6a6a63690298d5acf2a7b8351f95523d1a
Corsika1634 <corsika1634@gmail.com>
2021-11-24 19:52:13 +0200

    Initial commitZZ

---
+++ task.js
@@ -0,0 +1,18 @@
+
+const args = [null, null, 1, undefined, 0, 'null', NaN, ''];
+const symb = ',';
+
+function applyCustomJoin(arr, sep = '') {
+  let result = '';
+
+  for (let i = 0; i < arr.length - 1; i++) {
+        result += arr[i];
+        result += sep;
+  }
+  result += arr[arr.length - 1];
+  console.log(result);
+  return result;
+
+}
+
+applyCustomJoin(args, symb);
