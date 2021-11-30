22355019d3a53256bc05f5d6a056c62479438d82
Corsika1634 <corsika1634@gmail.com>
2021-11-30 21:54:09 +0200

    Practical lesson #2

--- task.js
+++ task.js
@@ -1,3 +1,6 @@
+// const user = prompt('Как вас зовут?', '');
+// alert(`Привет, ${user}!`);
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -8,14 +11,28 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов? ', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов? ', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов? ', ''),
+    b = prompt('На сколько оцените его?', '');
 
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман!');
+} else {
+  console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
 
