7c9769551d3845198326d30328a2c7faab680032
Corsika1634 <corsika1634@gmail.com>
2021-11-30 20:43:08 +0200

    Старт консольного приложения

--- task.js
+++ task.js
@@ -1,5 +1,24 @@
-const user = prompt('Как вас зовут?', '');
-alert(`Привет, ${user}!`);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt('Один из последних просмотренных фильмов? ', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов? ', ''),
+      d = prompt('На сколько оцените его?', '');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
 
 // const result = confirm('Are you here?');
 // console.log(result);
